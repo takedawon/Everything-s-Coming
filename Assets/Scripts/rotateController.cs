208e28009ff5944f7f211c670b7459cf7443fb08
takedawon <takedawon@naver.com>
2019-05-21 16:50:29 +0900

    장애물이 사용자로 날라오는 기능 추가
    날아오면서, 사용자가 피하면 장애물 Destory

--- Assets/Scripts/rotateController.cs
+++ Assets/Scripts/rotateController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class rotateController : MonoBehaviour
 {
+    GameObject obstacle;
+    Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("FPSController").transform.position;
+        obstacle = GameObject.Find("Halberd_A");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 360) * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target, 0.2f);
+        if(transform.position == target)
+        {
+            Destroy(this.obstacle);
+        }
     }
+
+
+
 }
