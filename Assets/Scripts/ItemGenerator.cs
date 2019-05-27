bf03070c023d1490f01fa2b1b377f3d04a02ff73
takedawon <takedawon@naver.com>
2019-05-27 21:50:55 +0900

    장애물 프리팹 스크립트 추가

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -8,19 +8,15 @@ public class ItemGenerator : MonoBehaviour
     float span = 1.0f;
     float delta = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-        this.delta += Time.deltaTime;
-        if(this.delta > this.span)
-        {
-            this.delta = 0;
-            Instantiate(halberdPrefab);
-        }
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            Instantiate(halberdPrefab);
+        }
     }
 }
--- Assets/Scripts/rotateController.cs
+++ Assets/Scripts/rotateController.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class rotateController : MonoBehaviour
 {
     GameObject obstacle;
+    GameObject terrain;
     Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("FPSController").transform.position;
         obstacle = GameObject.Find("Halberd_A");
+        terrain = GameObject.Find("Terrain");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 test = new Vector3(0, 2, 0);
         transform.Rotate(new Vector3(0, 0, 360) * Time.deltaTime);
-        transform.position = Vector3.MoveTowards(transform.position, target, 0.2f);
-        if(transform.position == target)
+        transform.position = Vector3.MoveTowards(transform.position, target - test, 0.2f);
+
+        if (transform.position == terrain.transform.position)
         {
             Destroy(this.obstacle);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other);
     }
 
 
