0e7d98c0ba25bd8337d1abae7bb26db433f73ee9
takedawon <takedawon@naver.com>
2019-05-27 20:49:21 +0900

    기존 맵 삭제 후, 맵 제작

---
+++ Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public GameObject halberdPrefab;
+    float span = 1.0f;
+    float delta = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span)
+        {
+            this.delta = 0;
+            Instantiate(halberdPrefab);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
