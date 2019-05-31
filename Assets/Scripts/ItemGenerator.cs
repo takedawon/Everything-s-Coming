cbd8b29e2e44d28af074c11e5b5b9704e56c8136
takedawon <takedawon@naver.com>
2019-05-31 21:33:14 +0900

    각 장애물들이 각각의 큐브로 이동하도록 설정

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -5,10 +5,70 @@ using UnityEngine;
 public class ItemGenerator : MonoBehaviour
 {
     public GameObject halberdPrefab;
+    public List<GameObject> cube = new List<GameObject>();
     float span = 5.0f;
     float delta = 0;
     // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cube1 = GameObject.Find("place1");
+        GameObject cube2 = GameObject.Find("place2");
+        GameObject cube3 = GameObject.Find("place3");
+        GameObject cube4 = GameObject.Find("place4");
+        GameObject cube5 = GameObject.Find("place5");
+        GameObject cube6 = GameObject.Find("place6");
+        GameObject cube7 = GameObject.Find("place7");
+        GameObject cube8 = GameObject.Find("place8");
+        GameObject cube9 = GameObject.Find("place9");
+        GameObject cube10 = GameObject.Find("place10");
+        GameObject cube11 = GameObject.Find("place11");
+        GameObject cube12 = GameObject.Find("place12");
+        GameObject cube13 = GameObject.Find("place13");
+        GameObject cube14 = GameObject.Find("place14");
+        GameObject cube15 = GameObject.Find("place15");
+        GameObject cube16 = GameObject.Find("place16");
+        GameObject cube17 = GameObject.Find("place17");
+        GameObject cube18 = GameObject.Find("place18");
+        GameObject cube19 = GameObject.Find("place19");
+        GameObject cube20 = GameObject.Find("place20");
+        GameObject cube21 = GameObject.Find("place21");
+        GameObject cube22 = GameObject.Find("place22");
+        GameObject cube23 = GameObject.Find("place23");
+        GameObject cube24 = GameObject.Find("place24");
+        GameObject cube25 = GameObject.Find("place25");
+        GameObject cube26 = GameObject.Find("place26");
+        GameObject cube27 = GameObject.Find("place27");
+        GameObject cube28 = GameObject.Find("place28");
 
+        cube.Add(cube1);
+        cube.Add(cube2);
+        cube.Add(cube3);
+        cube.Add(cube4);
+        cube.Add(cube5);
+        cube.Add(cube6);
+        cube.Add(cube7);
+        cube.Add(cube8);
+        cube.Add(cube9);
+        cube.Add(cube10);
+        cube.Add(cube11);
+        cube.Add(cube12);
+        cube.Add(cube13);
+        cube.Add(cube14);
+        cube.Add(cube15);
+        cube.Add(cube16);
+        cube.Add(cube17);
+        cube.Add(cube18);
+        cube.Add(cube19);
+        cube.Add(cube20);
+        cube.Add(cube21);
+        cube.Add(cube22);
+        cube.Add(cube23);
+        cube.Add(cube24);
+        cube.Add(cube25);
+        cube.Add(cube26);
+        cube.Add(cube27);
+        cube.Add(cube28);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -16,8 +76,9 @@ public class ItemGenerator : MonoBehaviour
         if (this.delta > this.span)
         {
             this.delta = 0;
-            for(int i=0; i<=16;i++) { 
+            for(int i=0; i<=27;i++) { 
                 Instantiate(halberdPrefab);
+                halberdPrefab.GetComponent<rotateController>().setPosition(1,cube[i].transform.position);
             }
         }
     }
--- Assets/Scripts/rotateController.cs
+++ Assets/Scripts/rotateController.cs
@@ -7,21 +7,33 @@ public class rotateController : MonoBehaviour
     GameObject obstacle;
     GameObject terrain;
     Vector3 target;
+    public GameObject cube;
+    public Vector3 testPosition = new Vector3();
+
+    public int test=0;
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.Find("FPSController").transform.position;
         obstacle = GameObject.Find("Halberd_A");
         terrain = GameObject.Find("Terrain");
+        cube = GameObject.Find("place1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 test = new Vector3(0, 2, 0);
-        transform.Rotate(new Vector3(0, 0, 360) * Time.deltaTime);
-        transform.position = Vector3.MoveTowards(transform.position, target - test, 0.2f);
+        if(test != 0) {
+            Vector3 test = new Vector3(0, 2, 0);
+            transform.Rotate(new Vector3(0, 0, 360) * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, testPosition, 0.1f);
+        }
+    }
 
+    public void setPosition(int a,Vector3 b)
+    {
+        test = a;
+        testPosition = b;
     }
 
 }
