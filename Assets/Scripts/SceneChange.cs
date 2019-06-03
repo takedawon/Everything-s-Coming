5f4444b3a0fdbaec7714543a52abfb405d6fdbdb
takedawon <takedawon@naver.com>
2019-06-03 16:10:27 +0900

    씬 전환 기능 추가

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void ChangeFirstScene()
+    {
+        SceneManager.LoadScene("FirstScenes");
+    }
+
+    public void ChanageGameScene()
+    {
+        SceneManager.LoadScene("GameScenes");
+    }
+}
